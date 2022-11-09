4192d443a080c8e404237f8948ec8eb014d2b89d
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-11-10 01:42:07 +0600

    load service data from server

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eqk1vsl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+
+    try {
+        const serviceCollection = client.db('infiniteSnaps').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+    }
+
+    finally {
+
+    }
+
+}
+
+run().catch(error => console.log(error));
 
 
 app.get('/', (req, res) => {
